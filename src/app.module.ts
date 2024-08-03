ad1e4ac4e3bd580f85ce95efb3a455f491d6992c
vincenter <vincenterr@yahoo.com.tw>
2024-08-03 09:46:07 +0800

    Install and configure the passport package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ArticlesModule, UsersModule],
+  imports: [ArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
